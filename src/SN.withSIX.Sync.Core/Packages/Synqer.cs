53c3aa8fe72c7bca659f930bebe481ba48d084c4
Patrick Roza <roza@sixnetworks.net>
2016-08-05 11:11:48 +0200

    Fix ext

--- src/SN.withSIX.Sync.Core/Packages/Synqer.cs
+++ src/SN.withSIX.Sync.Core/Packages/Synqer.cs
@@ -20,6 +20,7 @@ using SN.withSIX.Sync.Core.Packages.Internals;
 using SN.withSIX.Sync.Core.Repositories.Internals;
 using SN.withSIX.Sync.Core.Transfer;
 using SN.withSIX.Sync.Core.Transfer.MirrorSelectors;
+using withSIX.Api.Models.Extensions;
 
 namespace SN.withSIX.Sync.Core.Packages
 {
