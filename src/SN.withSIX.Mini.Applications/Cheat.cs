554a69fb84ed4243e0bdb1f8364f4344a618720d
Patrick Roza <roza@sixnetworks.net>
2016-09-12 10:21:20 +0200

    Bump syncver

--- src/SN.withSIX.Mini.Applications/Cheat.cs
+++ src/SN.withSIX.Mini.Applications/Cheat.cs
@@ -152,7 +152,7 @@ namespace SN.withSIX.Mini.Applications
         private static readonly Lazy<IPEndPoint> httpsAddress =
             new Lazy<IPEndPoint>(() => new IPEndPoint(srvAddress, ApiPort));
         public static IPEndPoint HttpsAddress => httpsAddress.Value;
-        public const int SyncVersion = 11;
+        public const int SyncVersion = 14;
 
         public static string InternalVersion { get; set; }
         public static string ProductVersion { get; set; }
