b24522de7f085493c3fcdf977098606fe655bd82
Patrick Roza <roza@sixnetworks.net>
2016-09-28 19:28:17 +0200

    Re add missing resolver

--- src/SN.withSIX.Mini.Infra.Api/Startup.cs
+++ src/SN.withSIX.Mini.Infra.Api/Startup.cs
@@ -274,5 +274,28 @@ namespace SN.withSIX.Mini.Infra.Api
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(returnValue.ToJson()).ConfigureAwait(false);
         }
+
+        internal class Resolver : DefaultParameterResolver
+        {
+            private readonly JsonSerializer _serializer;
+
+            private FieldInfo _valueField;
+
+            public Resolver(JsonSerializer serializer) {
+                _serializer = serializer;
+            }
+
+            public override object ResolveParameter(ParameterDescriptor descriptor, IJsonValue value) {
+                if (value.GetType() == descriptor.ParameterType)
+                    return value;
+
+                if (_valueField == null)
+                    _valueField = value.GetType().GetField("_value", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                var json = (string)_valueField.GetValue(value);
+                using (var reader = new StringReader(json))
+                    return _serializer.Deserialize(reader, descriptor.ParameterType);
+            }
+        }
     }
 }
