453cfcac9ae1e6ced1a0e73a94e861dfe72ae426
Patrick Roza <roza@sixnetworks.net>
2016-10-13 15:17:22 +0200

    Bump

--- src/withSIX.Core/Properties/AssemblyInfo.cs
+++ src/withSIX.Core/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.128")]
 [assembly: AssemblyFileVersion("1.0.0.128")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
 [assembly: InternalsVisibleTo("withSIX.Play.Tests.Core")]
 [assembly: InternalsVisibleTo("withSIX.Play.Tests.Presentation")]
