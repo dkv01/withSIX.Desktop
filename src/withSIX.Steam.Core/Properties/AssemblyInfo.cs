820b5969d4cc2b6ba74105ece1ff84eed3ced890
Patrick Roza <roza@sixnetworks.net>
2016-10-14 18:30:17 +0200

    Release updates

--- src/withSIX.Steam.Core/Properties/AssemblyInfo.cs
+++ src/withSIX.Steam.Core/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Resources;
 
 [assembly: AssemblyVersion("1.0.3.0")]
 [assembly: AssemblyFileVersion("1.0.3.0")]
-[assembly: AssemblyInformationalVersion("1.0.4")]
+[assembly: AssemblyInformationalVersion("1.0.5")]
